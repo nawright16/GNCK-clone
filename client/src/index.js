1a2612dc2fb849aef974e02eacd6ee3e0c374c1a
Nick Wright <nwright@egnyte.com>
2023-04-26 18:21:03 -0400

    added index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// Require the necessary dependencies
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config();  // Load environment variables from a .env file if it exists
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Set up middleware
+app.use(cors());             // Enable CORS (Cross-Origin Resource Sharing)
+app.use(express.json());     // Enable JSON parsing for request bodies
+
+// Define routes for the application
+
+//Welcome message route
+app.get("/", (req, res) => {
+    res.send("Welcome to our Todo API, where you can procrastinate in style!");
+});
+
+//Route to CREATE a new todo
+
+//Route to get READ all todos
+
+//Route to get READ a single todo by ID
+
+//Route to UPDATE a todo by ID
+
+//Route to DELETE a todo by ID
+
+app.listen(process.env.PORT, () => {
+    console.log(`Making a todo list on port: ${process.env.PORT} with the ALLSTARS Casey, Nick, Gregg, and Kristen`)
+});
 
 
