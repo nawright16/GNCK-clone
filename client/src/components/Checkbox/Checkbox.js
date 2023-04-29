b42bab741f26f873cfb43b72b847ca5e6caec9d1
Kristen von Gerichten <kristenvongerichten@kristens-mbp.lan>
2023-04-29 13:50:55 -0400

    created Checkbox toggle

--- client/src/components/Checkbox/Checkbox.js
+++ client/src/components/Checkbox/Checkbox.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { MDBSwitch } from 'mdb-react-ui-kit';
+import "./Checkbox.css";
 
 const Checkbox = ({ completed, onChange }) => {
   return (
-    <input
-      type="checkbox"
+
+    <MDBSwitch 
+      id='toggle' 
+      label=''
       checked={completed}
       onChange={onChange}
     />
@@ -11,4 +15,3 @@ const Checkbox = ({ completed, onChange }) => {
 };
 
 export default Checkbox;
-
