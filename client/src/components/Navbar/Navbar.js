d4799372f1e9248ae147cecb9ddd4abe53f60cd1
Nick Wright <nwright@egnyte.com>
2023-04-30 12:44:38 -0400

    added navbar css file. Modal seems to be working, but need to address the css.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,40 +1,42 @@
-import React, { useState, useRef, useEffect, Component } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { BsFillCaretDownFill } from 'react-icons/bs';
 import { BsFillCaretUpSquareFill } from 'react-icons/bs'
+import './Navbar.css'
 // import Dashboard from "../Dashboard/Dashboard"
 import Modal from "../Modal/Modal";
 
 const Navbar = () => {
-    const [navbarOpen, setNavbarOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-    const ref = useRef();
-    useEffect(() => {
-      const handler = (event) => {
-        if (
-          navbarOpen &&
-          ref.current &&
-          !ref.current.contains(event.target)
-        ) {
-          setNavbarOpen(false);
-        }
-      };
-      document.addEventListener('mousedown', handler);
-      return () => {
-        document.removeEventListener('mousedown', handler);
-      };
-    }, [navbarOpen]);
+  const ref = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (
+        navbarOpen &&
+        ref.current &&
+        !ref.current.contains(event.target)
+      ) {
+        setNavbarOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [navbarOpen]);
 
 
-return (
+  return (
     <nav ref={ref} className="navbar">
-        <button className="toggle" onClick={() => setNavbarOpen((prev) => !prev)}>
-            {navbarOpen ? (<BsFillCaretUpSquareFill style={{ width: '32px', height: '32px' }} />) : (<BsFillCaretDownFill style={{ width: '32px', height: '32px' }} />)}
-        </button>
-        <ul className={`menu-nav${navbarOpen ? ' show-menu' : <Modal />}`}>
-        </ul>
+      <button className="toggle" onClick={() => setNavbarOpen((prev) => !prev)}>
+        {navbarOpen ? (<BsFillCaretUpSquareFill style={{ width: '32px', height: '32px' }} />) : (<BsFillCaretDownFill style={{ width: '32px', height: '32px' }} />)}
+      </button>
+      <ul className={`menu-nav${navbarOpen ? <Modal /> : ''}`}>
+        <li><Modal /></li>
+      </ul>
 
     </nav>
-);
+  );
 
 };
 
