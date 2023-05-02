19b21eed951b24125a4fa8ce459c2d5dd6732cb0
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-05-02 14:30:22 -0400

    updated client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React, { Fragment, useState } from "react"; //groups list of children without adding extra nodes to the DOM
 import './App.css';
 
-
 //components
-
 import InputTodo from "./components/InputTodo/InputTodo";
 import ListTodos from "./components/ListTodos/ListTodos";
 import CompletedList from "./components/CompletedList/CompletedList";
-
-
-
 
 function App() {
   const [isCompletedTodoUpdated, setIsCompletedTodoUpdated] = useState(false);
