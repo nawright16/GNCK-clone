c872484fdd194501e9ad6d0fdf1d0f086cb39ef8
Kristen von Gerichten <kristenvongerichten@kristens-mbp.lan>
2023-05-03 19:35:54 -0400

    changed App.css and App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,37 @@
-import React, { Fragment, useState } from "react"; //groups list of children without adding extra nodes to the DOM
-import './App.css';
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
+import React, { Fragment, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
-//components
-
+// components
 import InputTodo from "./components/InputTodo/InputTodo";
 import ListTodos from "./components/ListTodos/ListTodos";
 import Sidebar from "./components/Sidebar/Sidebar";
-import About from './components/About/About'
+import About from "./components/About/About";
 
+function Header() {
+  return (
+    <header>
+      <Container>
+        <Nav defaultActiveKey="/" variant="tabs">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/">
+              GNCK
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/about">
+              About Us
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </header>
+  );
+}
 
-
-function App() {
+function Display() {
   const [isCompletedTodoUpdated, setIsCompletedTodoUpdated] = useState(false);
 
   const handleCompletedTodo = () => {
@@ -21,52 +39,33 @@ function App() {
   };
 
   return (
-
-    <div className="App">
-      <Router>
-        <header>
-          <Container>
-            <Nav defaultActiveKey="/" variant="tabs">
-              <Nav.Item>
-                <Nav.Link href="/">
-                  <Link to="/">GNCK</Link>
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item >
-                <Nav.Link eventKey={"aboutPage"}>
-                  <Link to="/about">About Us</Link>
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Container>
-
-        </header>
-
-        <div className="display">
-          <Routes>
-            
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </Router>
-
-      <body>
-    
-    <Fragment>
-      <div className="container">
-        <InputTodo />
-        <ListTodos onTodoCompleted={handleCompletedTodo} />
-        <Sidebar
-          isCompletedTodoUpdated={isCompletedTodoUpdated}
-          handleCompletedTodo={handleCompletedTodo} // Pass the function here
-        />
-      </div>
-    </Fragment>
-    </body>
-
+    <div className="display">
+      <Routes>
+        <Route path="/" element={<Fragment>
+          <InputTodo />
+          <ListTodos onTodoCompleted={handleCompletedTodo} />
+          <Sidebar
+            isCompletedTodoUpdated={isCompletedTodoUpdated}
+            handleCompletedTodo={handleCompletedTodo}
+          />
+        </Fragment>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
 
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <main>
+          <Display />
+        </main>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
