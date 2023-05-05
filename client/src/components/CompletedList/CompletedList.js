fda9d8ea1d26385ef60d6973185eada6e573b4be
Kristen von Gerichten <kristenvongerichten@kristens-mbp.lan>
2023-05-05 18:42:23 -0400

    updated MyCalendar, added icon to sidebar and sidebar css

--- client/src/components/CompletedList/CompletedList.js
+++ client/src/components/CompletedList/CompletedList.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import './CompletedList.css';
 import { BsXCircleFill } from 'react-icons/bs';
 
-
 const CompletedList = ({ isCompletedTodoUpdated }) => {
+  // Initialize state for completed tasks
   const [completedTasks, setCompletedTasks] = useState([]);
 
+  // Fetch completed tasks from the API when the component mounts or when a new completed todo is added
   useEffect(() => {
     fetch("http://localhost:5001/completed_todos")
       .then((response) => response.json())
       .then((data) => setCompletedTasks(data));
   }, [isCompletedTodoUpdated]);
 
+  // Handle the deletion of a completed task by sending a DELETE request to the API
   const handleDelete = (id) => {
     fetch(`http://localhost:5001/completed_todos/${id}`, {
       method: "DELETE",
@@ -26,6 +28,7 @@ const CompletedList = ({ isCompletedTodoUpdated }) => {
       .catch((error) => console.error(error));
   };
 
+  // Render the list of completed tasks and provide a delete button for each one
   return (
     <div className="completed">
       <h3>Completed Tasks</h3>
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -4,19 +4,27 @@ import "./Sidebar.css";
 import CompletedList from "../CompletedList/CompletedList";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 function Sidebar({ isCompletedTodoUpdated, todos, handleCompletedTodo }) {
+    // Set up state for whether the calendar is shown or hidden
     const [showCalendar, setShowCalendar] = useState(false);
-  
+
+    // Function to toggle the visibility of the calendar
     const handleToggleCalendar = () => {
-        setShowCalendar(!showCalendar); 
+        setShowCalendar(!showCalendar);
     };
-  
+
     return (
+        // Use template literal to conditionally add "show-calendar" class to container div
         <div className={`sidebar-container${showCalendar ? " show-calendar" : ""}`}>
+
+            {/* Button to toggle calendar visibility */}
             <button className="calendar-button" onClick={handleToggleCalendar}>
                 <FontAwesomeIcon icon={faCalendarAlt} />
             </button>
+
+            {/* Calendar component */}
             <div className="calendar-container">
                 {showCalendar && (
                     <MyCalendar
@@ -25,6 +33,13 @@ function Sidebar({ isCompletedTodoUpdated, todos, handleCompletedTodo }) {
                     />
                 )}
             </div>
+
+            {/* Icon to indicate slide-out functionality */}
+            <div className="caret-right">
+                <FontAwesomeIcon icon={faCaretRight} />
+            </div>
+
+            {/* Completed todo list component */}
             <div className="completed-list">
                 <CompletedList isCompletedTodoUpdated={isCompletedTodoUpdated} />
             </div>
